a5d137fc1263f31b93995a5082dc819453b8367f
Samo Prelog <sprelog@stackoverflow.com>
2015-04-09 22:47:22 +0200

    generate valid version numbers locally for easier testing

--- BuildAndPack.ps1
+++ BuildAndPack.ps1
@@ -5,7 +5,9 @@ get-childitem -file -recurse -include ("*.dll","*.exe","*.pdb","*.exe.config") |
     where { $_.Directory -match "bin\\Release" -and $_.Directory -notmatch "Test" -and $_.Directory -notmatch "StackExchange.Precompilation" } |
     foreach { ($_).CopyTo((get-item -Path ".\" -verbose).FullName + "\tools\" + $_.name, $true) }
 
+$date = Get-Date
+$version = "$($date.Year).$($date.Month).$($date.Day).$(($date.Hour * 60 + $date.Minute) * 60 + $date.Second)-alpha"
 new-item packages\obj -type directory -force
 get-childitem *.nuspec -Recurse | 
     where { $_.FullName -notmatch '\\packages\\' } | 
-    foreach { nuget pack $_.FullName -version 0.0-beta -o packages/obj -tool }
+    foreach { nuget pack $_.FullName -version $version -o packages\obj }
