6500c96e2693332fbfaf5e39f0f8c8fdb7cf4c2e
Samo Prelog <sprelog@stackoverflow.com>
2015-05-21 14:12:00 +0200

    verify we can build from the tools folder which has the same content as the .build nuget package

--- BuildAndPack.ps1
+++ BuildAndPack.ps1
@@ -2,6 +2,9 @@ msbuild /m /p:Configuration=Release /v:q /nologo
 
 .\PrepareForPack.ps1
 
+# make sure we can compile with the assemblies from the tools dir
+msbuild /m /t:Rebuild /p:Configuration=Debug /p:SECompilerPath=..\tools /v:q /nologo
+
 $epoch = [math]::truncate((new-timespan -start (get-date -date "01/01/1970") -end (get-date)).TotalSeconds)
 $version = "$semver-alpha$epoch"
 new-item .\packages\obj -type directory -force | out-null
