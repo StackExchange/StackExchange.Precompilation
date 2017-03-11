97276849547a5b1f5fdcb26e00d649c8e3759e0b
Samo Prelog <sprelog@stackoverflow.com>
2017-03-11 03:27:46 +0100

    we want roslyn v2

--- StackExchange.Precompilation.Build/CompilationAssemblyResolver.cs
+++ StackExchange.Precompilation.Build/CompilationAssemblyResolver.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
@@ -17,28 +19,64 @@ namespace StackExchange.Precompilation
                   typeof(CompilationAssemblyResolver).FullName) as CompilationAssemblyResolver;
             resolver.RegisterDomain(domain);
             resolver.Setup(references);
-
         }
-        private Dictionary<string, Lazy<Assembly>> fullLookup;
+
         private AppDomain domain;
+        private readonly ConcurrentBag<Tuple<AssemblyName, Func<Assembly>>> assemblyLoaders = new ConcurrentBag<Tuple<AssemblyName, Func<Assembly>>>();
+        private readonly ConcurrentDictionary<string, Lazy<Assembly>> resolvedAssemblies = new ConcurrentDictionary<string, Lazy<Assembly>>();
+
         private void Setup(string[] references)
         {
-            var referenceAssemblies = references.Select(x =>
+            void Resolve(AssemblyName name, Func<Assembly> loader)
             {
-                try
-                {
-                    return Assembly.ReflectionOnlyLoadFrom(x);
-                }
-                catch (Exception ex)
-                {
-                    return null;
-                }
-            }).Where(x => x != null).ToArray();
+                assemblyLoaders.Add(Tuple.Create(name, loader));
 
-            fullLookup = new Tuple<string, string>[] { }
-                .Concat(referenceAssemblies.Select(x => Tuple.Create(x.FullName, x.Location)))
-                .Concat(referenceAssemblies.ToLookup(x => x.GetName().Name).Select(x => Tuple.Create(x.Key, x.First().Location)))
-                .ToDictionary(x => x.Item1, x => new Lazy<Assembly>(() => Assembly.LoadFile(x.Item2), LazyThreadSafetyMode.ExecutionAndPublication));
+                var keyName = new AssemblyName(ApplyPolicy(name.FullName));
+                resolvedAssemblies.AddOrUpdate(keyName.FullName, ResolvedAssembly(loader), (key, existing) => existing); // TODO log conflicting binds?
+                resolvedAssemblies.AddOrUpdate(keyName.Name, ResolvedAssembly(loader), (key, existing) => existing); // TODO log conflicting partial binds?
+            }
+
+            // load the embedded compile-time references, we're gonna need them for sure
+            const string prefix = "embedded_ref://";
+            var thisAssembly = typeof(CompilationAssemblyResolver).Assembly;
+            thisAssembly.GetManifestResourceNames()
+                .AsParallel()
+                .Where(x => x.StartsWith(prefix))
+                .ForAll(resourceKey => 
+                {
+                    using (var resource = thisAssembly.GetManifestResourceStream(resourceKey))
+                    using (var ms = new MemoryStream())
+                    {
+                        if (resource != null)
+                        {
+                            resource.CopyTo(ms);
+                            var rawAssembly = ms.ToArray();
+                            var assembly = Assembly.Load(rawAssembly);
+                            var name = assembly.GetName();
+                            name.CodeBase = resourceKey;
+                            Resolve(name, () => assembly);
+                        }
+                    }
+                });
+
+            // load all the other references
+            references
+                .AsParallel()
+                .Select(x =>
+                {
+                    try
+                    {
+                        return AssemblyName.GetAssemblyName(x);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"warning: Couldn't load reference from '{x}' - '{ex.Message}'");
+                        return null;
+                    }
+                })
+                .Where(x => x != null)
+                .ForAll(name => Resolve(name, () => Assembly.LoadFile(new Uri(name.CodeBase).LocalPath)));
+
         }
 
         private void RegisterDomain(AppDomain domain)
@@ -47,16 +85,21 @@ namespace StackExchange.Precompilation
             this.domain.AssemblyResolve += ResolveAssembly;
         }
 
+        private string ApplyPolicy(string name)
+        {
+            while (true) {
+                var newName = domain.ApplyPolicy(name);
+                if (newName == name) return name;
+                name = newName;
+            }
+        }
+
         private Assembly ResolveAssembly(object sender, ResolveEventArgs e)
         {
-            var name = domain.ApplyPolicy(e.Name);
-            Lazy<Assembly> a;
-            if ((fullLookup.TryGetValue(name, out a)))
-            {
-                return a.Value;
-            }
+            var name = ApplyPolicy(e.Name);
 
-            return null;
+            return resolvedAssemblies.GetOrAdd(name, key => new Lazy<Assembly>(() => null, false)).Value;
         }
+        private static Lazy<Assembly> ResolvedAssembly(Func<Assembly> loader) => new Lazy<Assembly>(loader, LazyThreadSafetyMode.ExecutionAndPublication);
     }
 }
