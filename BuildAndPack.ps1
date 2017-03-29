92bca4e169d2db8eaf4d16258d347fa8a36426f0
Samo Prelog <sprelog@stackoverflow.com>
2017-03-29 16:05:58 +0200

    adding a VersionSuffix parameter to the build script

--- BuildAndPack.ps1
+++ BuildAndPack.ps1
@@ -1,12 +1,26 @@
+param(
+    [parameter(Position=0)]
+    [string] $VersionSuffix
+)
+
 msbuild /m /p:Configuration=Release /v:q /nologo
 
 .\PrepareForPack.ps1
+$epoch = [math]::truncate((new-timespan -start (get-date -date "01/01/1970") -end (get-date)).TotalSeconds)
+if ($VersionSuffix)
+{
+    $version = "$semver$VersionSuffix"
+    Write-Host "Packing explicit version $version"
+}
+else
+{
+    $version = "$semver-local$epoch"
+    Write-Host "Packing local package $version"
+}
 
 # make sure we can compile with the assemblies from the tools dir
 msbuild /m /t:Rebuild /p:Configuration=Debug /p:SECompilerPath=..\tools /v:q /nologo
 
-$epoch = [math]::truncate((new-timespan -start (get-date -date "01/01/1970") -end (get-date)).TotalSeconds)
-$version = "$semver-alpha$epoch"
 new-item .\packages\obj -type directory -force | out-null
 get-childitem *.nuspec -Recurse | 
     where { $_.FullName -notmatch '\\packages\\' } | 
