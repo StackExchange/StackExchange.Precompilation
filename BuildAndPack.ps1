e9f2de5fd2fae22276932a79c3baf0616ed06013
Samo Prelog <sprelog@stackoverflow.com>
2017-03-29 16:21:56 +0200

    moar build tweaks, hello appveyor

--- BuildAndPack.ps1
+++ BuildAndPack.ps1
@@ -2,10 +2,8 @@ param(
     [parameter(Position=0)]
     [string] $VersionSuffix
 )
+set -name semver -scope global -value (get-content .\semver.txt)
 
-msbuild /m /p:Configuration=Release /v:q /nologo
-
-.\PrepareForPack.ps1
 $epoch = [math]::truncate((new-timespan -start (get-date -date "01/01/1970") -end (get-date)).TotalSeconds)
 if ($VersionSuffix)
 {
@@ -17,6 +15,23 @@ else
     $version = "$semver-local$epoch"
     Write-Host "Packing local package $version"
 }
+
+if ($env:AppVeyor)
+{
+    Update-AppveyorBuild -Version $version
+}
+
+write-host "##teamcity[setParameter name='semver' value='$semver']"
+
+msbuild /m /p:Configuration=Release /v:q /nologo
+
+new-item tools -type directory -force -ea stop
+
+get-childitem -file -recurse -include ("StackExchange.Precompiler.*") |
+     where { $_.Directory -match "bin\\Release" -and $_.FullName -notmatch "Test" } |
+     select -ExpandProperty FullName |
+     copy-item -destination ((get-item -Path ".\" -verbose).FullName + "\tools\") -verbose -ea stop
+
 
 # make sure we can compile with the assemblies from the tools dir
 msbuild /m /t:Rebuild /p:Configuration=Debug /p:SECompilerPath=..\tools /v:q /nologo
