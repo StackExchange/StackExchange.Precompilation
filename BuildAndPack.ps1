6daf10f47004ed97f37428ab726c2c5201fea5c0
Samo Prelog <sprelog@stackoverflow.com>
2017-03-30 13:17:23 +0200

    cleaning up build script

--- BuildAndPack.ps1
+++ BuildAndPack.ps1
@@ -27,7 +27,7 @@ if(-not $GitCommitId)
     $GitCommitId = $(git rev-parse HEAD)
 }
 
-$defaultArgs = "/m", "/nologo", "/p:SolutionDir=$((Resolve-Path .))\"
+$defaultArgs = "/v:q", "/m", "/nologo", "/p:SolutionDir=$((Resolve-Path .))\", "/t:Build", "/p:Configuration=Release"
 if ($MsBuildArgs)
 {
     $defaultArgs += $MsBuildArgs
@@ -36,8 +36,6 @@ if ($MsBuildArgs)
 
 $buildArgs =
     ".\StackExchange.Precompilation.Tests\StackExchange.Precompilation.Tests.csproj",
-    "/t:Build",
-    "/p:Configuration=Release",
     "/p:GitCommitId=$GitCommitId",
     "/p:PdbGitEnabled=true;PdbGitSkipVerify=true;PdbGitGitRemoteUrl=https://raw.githubusercontent.com/StackExchange/StackExchange.Precompilation"
 & "msbuild" $($buildArgs + $defaultArgs)
@@ -49,8 +47,8 @@ if ($LASTEXITCODE -ne 0)
 
 new-item tools -type directory -force -ea stop | out-null
 get-childitem -file -recurse -include ("StackExchange.Precompiler.*") |
-     where { $_.Directory -match "bin\\Release" -and $_.FullName -notmatch "Test" } |
-     select -ExpandProperty FullName |
+     where-object { $_.Directory -match "bin\\Release" -and $_.FullName -notmatch "Test" } |
+     select-object -ExpandProperty FullName |
      copy-item -destination ((get-item -Path ".\" -verbose).FullName + "\tools\") -verbose -ea stop
 
 
@@ -59,10 +57,7 @@ get-childitem -file -recurse -include ("StackExchange.Precompiler.*") |
     Write-Host "Testing $_"
     $testBuildArgs =
         "$_\$_.csproj",
-        "/t:Build",
-        "/v:q",
-        "/p:SECompilerPath=..\tools",
-        "/p:Configuration=Release"
+        "/p:SECompilerPath=..\tools"
     & "msbuild" $($testBuildArgs + $defaultArgs)
 
     if ($LASTEXITCODE -ne 0)
@@ -74,6 +69,6 @@ get-childitem -file -recurse -include ("StackExchange.Precompiler.*") |
 Write-Host "Packing"
 new-item .\packages\obj -type directory -force | out-null
 get-childitem *.nuspec -Recurse | 
-    where { $_.FullName -notmatch '\\packages\\' } | 
-    foreach { .nuget\nuget pack $_.FullName -version $version -o packages\obj } | 
+    where-object { $_.FullName -notmatch '\\packages\\' } |
+    foreach-object { .nuget\nuget pack $_.FullName -version $version -o packages\obj } |
     out-null
