407bdff29d12acd84fae106fcafe6652dd1fbdfe
Samo Prelog <sprelog@stackoverflow.com>
2017-03-30 13:12:26 +0200

    a bit more explicit build, don't try to build debug with release, since the precompilation assemblies won't match

--- BuildAndPack.ps1
+++ BuildAndPack.ps1
@@ -26,26 +26,28 @@ if(-not $GitCommitId)
 {
     $GitCommitId = $(git rev-parse HEAD)
 }
-if (-not $MsBuildArgs)
+
+$defaultArgs = "/m", "/nologo", "/p:SolutionDir=$((Resolve-Path .))\"
+if ($MsBuildArgs)
 {
-    $MsBuildArgs = @()
+    $defaultArgs += $MsBuildArgs
 }
 
-$MsBuildArgs += "/m", "/nologo"
 
 $buildArgs =
+    ".\StackExchange.Precompilation.Tests\StackExchange.Precompilation.Tests.csproj",
     "/t:Build",
     "/p:Configuration=Release",
     "/p:GitCommitId=$GitCommitId",
     "/p:PdbGitEnabled=true;PdbGitSkipVerify=true;PdbGitGitRemoteUrl=https://raw.githubusercontent.com/StackExchange/StackExchange.Precompilation"
-& "msbuild" $($buildArgs + $MsBuildArgs)
+& "msbuild" $($buildArgs + $defaultArgs)
 
 if ($LASTEXITCODE -ne 0)
 {
     exit $LASTEXITCODE
 }
 
-new-item tools -type directory -force -ea stop
+new-item tools -type directory -force -ea stop | out-null
 get-childitem -file -recurse -include ("StackExchange.Precompiler.*") |
      where { $_.Directory -match "bin\\Release" -and $_.FullName -notmatch "Test" } |
      select -ExpandProperty FullName |
@@ -53,17 +55,23 @@ get-childitem -file -recurse -include ("StackExchange.Precompiler.*") |
 
 
 # make sure we can compile with the assemblies from the tools dir
+"Test.WebApp", "Test.ConsoleApp" | foreach-object{
+    Write-Host "Testing $_"
+    $testBuildArgs =
+        "$_\$_.csproj",
+        "/t:Build",
+        "/v:q",
+        "/p:SECompilerPath=..\tools",
+        "/p:Configuration=Release"
+    & "msbuild" $($testBuildArgs + $defaultArgs)
 
-$testBuildArgs =
-    "/t:Rebuild",
-    "/p:SECompilerPath=..\tools",
-    "/p:Configuration=Debug"
-& "msbuild" $($testBuildArgs + $MsBuildArgs)
-if ($LASTEXITCODE -ne 0)
-{
-    exit $LASTEXITCODE
+    if ($LASTEXITCODE -ne 0)
+    {
+        exit $LASTEXITCODE
+    }
 }
 
+Write-Host "Packing"
 new-item .\packages\obj -type directory -force | out-null
 get-childitem *.nuspec -Recurse | 
     where { $_.FullName -notmatch '\\packages\\' } | 
